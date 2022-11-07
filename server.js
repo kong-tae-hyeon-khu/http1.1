eafe77e3533327d419e839d9cdc5c419061f7c2c
kongtaehyeon-khu <kong3047@khu.ac.kr>
2022-11-07 23:33:51 +0900

    Represet Basic Client(Request)  Information

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Open Server");
+  // Basic Client Information
+  console.log("::Client Address : %s", req.socket.address().address);
+  console.log("::Client Port : %s", req.socket.address().port);
+
+  console.log("::Request Version : HTTP/%s", req.httpVersion);
+  console.log("::Request Path : %s", req.url);
+  console.log(":: Request Command : %s", req.method);
 });
 
-server.listen(8000);
+server.listen(8080, (req, res) => {
+  console.log("HTTP server started at http://localhost:8080");
+});
+
+// GET Method
