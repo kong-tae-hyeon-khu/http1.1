fd1d37b35e9bf50e0fcd39fb9031a7390f165c9b
kongtaehyeon-khu <kong3047@khu.ac.kr>
2022-12-04 23:27:32 +0900

    Implement Method for POST and Response about POST request

--- method.js
+++ method.js
@@ -1,3 +1,4 @@
+// 곱하기 연산을 위한 GET Method 를 위한 함수.
 const method_for_get = (parameter) => {
   try {
     const urlParams = new URLSearchParams(parameter);
@@ -8,10 +9,34 @@ const method_for_get = (parameter) => {
     const multiple_result = var1 * var2;
 
     const result = `## GET request for calculation => ${var1} x ${var2} = ${multiple_result}`;
+
     return result;
   } catch (error) {
     console.log(`오류 내용 : ${error}`);
   }
 };
+// Client 의 정보를 표현하도록 해주는 함수 작성.
+const basic_info = (req) => {
+  console.log("::Client Address : %s", req.socket.address().address);
+  console.log("::Client Port : %s", req.socket.address().port);
 
+  console.log("::Request Version : HTTP/%s", req.httpVersion);
+  console.log("::Request Path : %s", req.url);
+  console.log(":: Request Command : %s", req.method);
+};
+
+const method_for_post = (request_body) => {
+  request_body = JSON.parse(request_body);
+
+  var1 = request_body.var1;
+  var2 = request_body.var2;
+  result = var1 * var2;
+  console.log(`## POST request data => var1=${var1}&var2=${var2}`);
+  console.log(
+    `## POST request for calculation => ${var1} x ${var2} = ${result}`
+  );
+};
+
+module.exports.basic_info = basic_info;
 module.exports.method_for_get = method_for_get;
+module.exports.method_for_post = method_for_post;
--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const http = require("http");
+const { parse } = require("querystring");
 const { URLSearchParams } = require("url");
 
 const test = require("./method");
 
 const server = http.createServer((req, res) => {
-  // Basic Client Information
-
-  console.log("::Client Address : %s", req.socket.address().address);
-  console.log("::Client Port : %s", req.socket.address().port);
-
-  console.log("::Request Version : HTTP/%s", req.httpVersion);
-  console.log("::Request Path : %s", req.url);
-  console.log(":: Request Command : %s", req.method);
-
+  // GET Method
   if (req.method === "GET") {
     if (req.url.includes("?")) {
-      console.log("## GET response [Start] for Calculation");
-      var calculated = req.url.split("?");
-      calculated = calculated[1];
+      // Basic Client Request Information
+      test.basic_info(req);
 
+      var calculated = req.url.split("?")[1];
+      console.log("## GET response [Start] for Calculation");
       var result = test.method_for_get(calculated);
       console.log(result);
 
+      res.end(result);
       console.log("## GET response [End] for Calculation");
+    } else if (req.url !== "/favicon.ico") {
+      // Basic Client Request Information
+      test.basic_info(req);
     }
+    // POST Method
+  } else if (req.method === "POST") {
+    // Basic Client Request Information
+    test.basic_info(req);
+
+    request_body = "";
+    req.on("data", (request_client) => {
+      request_body += request_client;
+      test.method_for_post(request_body);
+    });
   }
 });
 
 server.listen(8080, (req, res) => {
   console.log("HTTP server started at http://localhost:8080");
 });
-
-// GET Method
